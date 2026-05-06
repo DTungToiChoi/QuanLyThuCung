46cd6fcdd352132ae24d5c744157a5a00b212368
Gnart-doudou <thuytrang271204@gmail.com>
2026-05-06 21:43:21 +0700

    fix add

--- fe/vite.config.mjs
+++ fe/vite.config.mjs
@@ -1,6 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'node:url'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
+      { find: '@configs', replacement: fileURLToPath(new URL('./src/config', import.meta.url)) },
+      { find: '@shared', replacement: fileURLToPath(new URL('./src/shared', import.meta.url)) },
+      { find: '@utils', replacement: fileURLToPath(new URL('./src/shared/utils', import.meta.url)) },
+      { find: '@constants', replacement: fileURLToPath(new URL('./src/shared/constants', import.meta.url)) },
+      { find: '@assets', replacement: fileURLToPath(new URL('./src/assets', import.meta.url)) },
+      { find: '@apps', replacement: fileURLToPath(new URL('./src/apps', import.meta.url)) }
+    ]
+  }
 })
